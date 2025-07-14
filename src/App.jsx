3ef50060b5a95b45b55bf59c54856f75626f2631
Sreecharan <112425478+sreecharanVemula@users.noreply.github.com>
2025-07-14 12:27:01 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" >
+      <h1>SreeCharan</h1>
+    </div>
+  );
+}
+
+export default App;
